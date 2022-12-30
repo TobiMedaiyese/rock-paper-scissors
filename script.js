1f15bf72957eca56fa27b78cedfd3efeddad395a
Tobi Medaiyese <tobimedaiyese@gmail.com>
2022-12-30 12:29:27 -0600

    console log version

--- script.js
+++ script.js
@@ -9,38 +9,109 @@ function getComputerChoice() {
 let playerScore = 0
 let computerScore = 0
 
-const computerSelection = getComputerChoice();
+
 let playerSelection;
-function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt("rock, paper, or scissors?")
-    if (playerSelection === "rock" && computerSelection === "paper") {
+
+
+function playRoundRock() {
+    let computerSelection = getComputerChoice();
+    if (computerSelection === "paper") {
         computerScore++
         return "you lose"
-    } else if (playerSelection === computerSelection) {
-        return "It's a tie!"
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    } else if (computerSelection === "scissors"){
         playerScore++
-        return "player wins!"
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore++
-        return "You win"
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore++
-        return "You lose"
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore++
-        return "You lose"
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore++
-        return "You win"
-    } 
+        return "you win"
+    } else if (computerSelection === "rock"){
+        return "it's a tie"
+    }
+    
 }
 
-function game() {
-    for (let x = 1; x <= 5; x++) { 
-    console.log(playRound(playerSelection, computerSelection))
+function playRoundPaper() {
+    let computerSelection = getComputerChoice();
+    if (computerSelection === "paper") {
+        computerScore++
+        return "tie"
+    } else if (computerSelection === "scissors"){
+        playerScore++
+        return "you lose"
+    } else if (computerSelection === "rock"){
+        return "win"
+    }
+    
+}
+
+function playRoundScissors() {
+    let computerSelection = getComputerChoice();
+    if (computerSelection === "paper") {
+        computerScore++
+        return "you win"
+    } else if (computerSelection === "scissors"){
+        playerScore++
+        return "tie"
+    } else if (computerSelection === "rock"){
+        return "you lose"
+    }
+    
+}
+
+const btn = document.querySelector('.button1');
+btn.addEventListener('click', gameRock);
+
+const btn2 = document.querySelector('.button2');
+btn2.addEventListener('click', gamePaper);
+
+const btn3 = document.querySelector('.button3');
+btn3.addEventListener('click', gameScissors);
+
+function gameRock() {
+    console.log(playRoundRock())
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5 || computerScore === 5) {
+            if (playerScore > computerScore){
+                console.log("Player wins")
+                disableButtons()
+            } else if (playerScore < computerScore) {
+                console.log("Computer Wins")
+                disableButtons()
+            }
+        } 
     }
     console.log(`Player score: ${playerScore} | Computer score: ${computerScore}`)
-}
+    }
 
-console.log(game())
+function gamePaper() {
+    console.log(playRoundPaper())
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore > computerScore){
+            console.log("Player wins")
+            disableButtons()
+        } else if (playerScore < computerScore) {
+            console.log("Computer Wins")
+            disableButtons()
+        }  
+    }
+     console.log(`Player score: ${playerScore} | Computer score: ${computerScore}`)
+    }
+
+function gameScissors() {
+    console.log(playRoundScissors())
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5 || computerScore === 5) {
+            if (playerScore > computerScore){
+                console.log("Player wins")
+                disableButtons()
+            } else if (playerScore < computerScore) {
+                console.log("Computer Wins") 
+                disableButtons()
+            }
+        }
+    }
+    console.log(`Player score: ${playerScore} | Computer score: ${computerScore}`)
+    }
+
+    function disableButtons(){
+        btn.disabled = true;
+        btn2.disabled = true;
+        btn3.disabled = true;
+    }
